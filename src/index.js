804ba9999d00d8ec3b1427e5333313d175a3ff57
snahmd <sanahmed7@gmail.com>
2022-11-29 17:53:43 +0100

    master comment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
+//master
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
-  
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
