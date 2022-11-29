4c368c7d953626dfb7b0d94eecadc3acc22726f4
snahmd <sanahmed7@gmail.com>
2022-11-29 17:55:00 +0100

    day-1 comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// day-1
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
